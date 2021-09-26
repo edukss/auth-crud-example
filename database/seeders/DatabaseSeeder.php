7a5ec6f3f0e645942ec6d3ac87232233611cc71e
Edu <edukasas91@gmail.com>
2021-09-26 17:50:26 +0200

    add seeder Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User; 
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $user = new User();
+
+        $user->name = 'Admin';
+        $user->email = 'admin@admin.com';
+        $user->password = 'admin';
+        $user->is_admin = 1;
+
+        $user->save();
     }
 }
